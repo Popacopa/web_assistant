8d57c9a10874e349abcd032942a047242229b5a1
popacopa <paushakow@gmail.com>
2025-08-11 15:02:31 +0300

    server.cpp

--- server.cpp
+++ server.cpp
@@ -6,56 +6,61 @@
 #include "modules/settings.cpp"
 #include "modules/Signal.cpp"
 
-#define INFO(x): LOGI << x << "\n";
-#define ERROR(x): LOGE << x << "\n";
-#define DEBUG(x): LOGD << x << "\n";
-#define CRITICAL(x): LOGC << x << "\n";
-#define WARN(x): LOGW << x << "\n";
+
+/* файл сервера, ретранслирующего сигналы из сети 
+на ардуино и принимающего ответ от нее */
+
+
+#define INFO(x): LOGI << x << "\n";             //
+#define ERROR(x): LOGE << x << "\n";            //
+#define DEBUG(x): LOGD << x << "\n";            // псевдонимы логирования
+#define CRITICAL(x): LOGC << x << "\n";         //
+#define WARN(x): LOGW << x << "\n";             //
 
 
 Byte arduino_request(0b00); // 0b00 - default value, 0b01 - high, 0b10 - low, 0b11 - error
-Byte arduino_response;
+Byte arduino_response;      // ответ ардуины
 
-int opt;
-bool logging{0};
-std::string ip_address{"127.0.0.1"};
-int port{5555};
-std::string last_request;
-int last_status;
+int opt;            // переменная для парса аргументов при запуске сервера
+bool logging{0};    // булевая единица логирования
+std::string ip_address{"127.0.0.1"}; // адрес сервера по умолчанию
+int port{5555};     // порт сервера по умолчанию
+std::string last_request; // последнее полученное значение запроса
+int last_status;    // послединй статус порта
 
 
 int main(int argc, char const *argv[])
 {
 
-    Settings::setMainLogStream();
-    httplib::Server svr;
+    Settings::setMainLogStream(); // устанавливаем основной поток логирования (консоль)
+    httplib::Server svr;          // создаем сервер
 
-    while ((opt = getopt(argc, (char**)argv, "huli:p:")) != -1) {
-        switch (opt) {
-            case 'i':   ip_address = optarg; break;
-            case 'p':   port = std::stoi(optarg); break;
-            case 'l':   logging = true; break;
-            case 'h':   std::cout << Settings::helpLine << "\n"; return 0;
-            case '?':   std::cerr << "type [-h] to get help for this program\n"; return 1;
-            default:    std::cerr << "type [-h] to get help for this program\n"; return 1;
+    while ((opt = getopt(argc, (char**)argv, "huli:p:")) != -1) {                           //
+        switch (opt) {                                                                      //
+            case 'i':   ip_address = optarg; break;                                         //
+            case 'p':   port = std::stoi(optarg); break;                                    // парс параментров запуска сервера
+            case 'l':   logging = true; break;                                              //
+            case 'h':   std::cout << Settings::helpLine << "\n"; return 0;                  //
+            case '?':   std::cerr << "type [-h] to get help for this program\n"; return 1;  //
+            default:    std::cerr << "type [-h] to get help for this program\n"; return 1;  //
         }
     }
 
-    std::string device_port = ChoosePort::choose_port();
-    ComPort arduino(device_port);     
+    std::string device_port = ChoosePort::choose_port(); // выбор порта для ардуино
+    ComPort arduino(device_port);     //создает обьект порта для ардуино
 
-    if (logging) {Settings::setFileLogging();}
+    if (logging) {Settings::setFileLogging();}    // если логирование включено, то устанавливаем поток логирования в файл
 
-                                                                LOGI << "selected address: " << ip_address << ":" << port << "\t--start\n";
+                                                                LOGI << "selected address: " << ip_address << ":" << port << "\t--start\n"; 
 
-    svr.Post("/api", [&](const httplib::Request &req, httplib::Response &res) {
-        if (req.body != last_request) {
+    svr.Post("/api", [&](const httplib::Request &req, httplib::Response &res) {    //функция обработки POST запроса на /api
+        if (req.body != last_request) {                 // если тело запроса не равно последнему запросу, то обрабатываем его
             
-                if (last_status) {arduino.reopen();}
+                if (last_status) {arduino.reopen();}    // попытка открыть порт при возникновении ошибки с ним
 
-                int status;
+                int status;                             // переменная статуса трансляции сигнала
 
-                if (req.body == "1") {
+                if (req.body == "1") {                  //при 1 выставляется высокий первый бит сообщения
                     arduino_request.setBit(1, 0);                                  
                 } else {
                     arduino_request.clearBit(1, 0);
@@ -63,17 +68,17 @@ int main(int argc, char const *argv[])
                 
                 status = arduino.write(arduino_request.get_data());
 
-                res.set_content(status ? "sorry, port is not open" : req.body, "text/plain");
-                last_request = req.body;
+                res.set_content(status ? "sorry, port is not open" : req.body, "text/plain"); // формирование ответа на сервер
+                last_request = req.body;                // обновление последнего запроса
 
-                last_status = status;
+                last_status = status;              // обновление статуса порта
 
                                                                 LOGI << " POST " << req.body << "\t"
                                                                             << req.remote_addr << "\t" 
                                                                             << "status: " << status << "\n";
                 
-                arduino_response.set_data(arduino.read());
-                arduino_response.print(); 
+                arduino_response.set_data(arduino.read()); //чтение ответа от ардуино
+                arduino_response.print(); // печать ответа в логи
         }
     });
     
@@ -81,7 +86,7 @@ int main(int argc, char const *argv[])
     
         try
         {
-            svr.listen(ip_address, port);
+            svr.listen(ip_address, port); // запуск сервера на указанном адресе и порту
         }
         catch(...)
         {
